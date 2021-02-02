7c31c77b3fdf74ecc16a296155c6869fbc108280
PeAlonso-byte <PeAlonso-byte@users.noreply.github.com>
2021-02-02 13:43:44 +0100

    - Añadido Cuadro Modal
     - Se ha añadido cuadro modal.
     - Se ha añadido el dibujo de una
        función como prueba

--- Interfaces_GII/MainWindow.xaml.cs
+++ Interfaces_GII/MainWindow.xaml.cs
@@ -20,9 +20,147 @@ namespace Interfaces_GII
     /// </summary>
     public partial class MainWindow : Window
     {
+        double ancho, alto;
+        Polyline linea, lineaX, lineaY;
+        private CuadroModal cdm;
         public MainWindow()
         {
             InitializeComponent();
+            ancho = 0;
+            alto = 0;
+            linea = null;
+            lineaX = null;
+            lineaY = null;
+            cdm = null;
         }
+
+        /* METODOS PARA DIBUJAR LA GRAFICA */
+        private void btnDibujo_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String)btnDibujo.Content == "Dibujar")
+            {
+                dibuja();
+                dibuja_ejes();
+                btnDibujo.Content = "Limpiar";
+            }
+            else
+            {
+                miCanvas.Children.Clear();
+                btnDibujo.Content = "Dibujar";
+            }
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (linea != null)
+            {
+                if ((String)btnDibujo.Content == "Limpiar")
+                {
+                    miCanvas.Children.Clear();
+                    dibuja();
+                    dibuja_ejes();
+                }
+            }
+        }
+
+        private void dibuja()
+        {
+            Point p;
+            double x;
+
+            ancho = miCanvas.ActualWidth;
+            alto = miCanvas.ActualHeight;
+
+            if (linea == null)
+            {
+                linea = new Polyline();
+                linea.Stroke = Brushes.Red;
+                linea.StrokeThickness = 2;
+
+            }
+            else
+            {
+                linea.Points.Clear();
+            }
+
+            for (x = -ancho / 2 + ancho / 22; x <= ancho / 2 - ancho / 22; x++)
+            {
+                p = new Point();
+                p.X = x + ancho / 2;
+                p.Y = alto * (100 - ((x * 22 / ancho) * (x * 22 / ancho))) / 110;
+                linea.Points.Add(p);
+            }
+
+            miCanvas.Children.Add(linea);
+        }
+
+        private void dibuja_ejes()
+        {
+            Point p;
+            double x;
+
+            ancho = miCanvas.ActualWidth;
+            alto = miCanvas.ActualHeight;
+            if (lineaX == null)
+            {
+                lineaX = new Polyline();
+                lineaX.Stroke = Brushes.Black;
+                lineaX.StrokeThickness = 2;
+
+            }
+            else
+            {
+                lineaX.Points.Clear();
+            }
+            for (x = 0; x <= ancho; x++)
+            {
+                p = new Point();
+                p.X = x;
+                p.Y = alto / 2;
+                lineaX.Points.Add(p);
+            }
+
+            if (lineaY == null)
+            {
+                lineaY = new Polyline();
+                lineaY.Stroke = Brushes.Black;
+                lineaY.StrokeThickness = 2;
+
+            }
+            else
+            {
+                lineaY.Points.Clear();
+            }
+            for (x = 0; x <= ancho; x++)
+            {
+                p = new Point();
+                p.X = ancho / 2;
+                p.Y = x;
+                lineaY.Points.Add(p);
+            }
+
+            miCanvas.Children.Add(lineaX);
+            miCanvas.Children.Add(lineaY);
+
+        }
+
+        /* FIN METODOS PARA DIBUJAR LA GRAFICA */
+
+        /* METODOS CUADRO MODAL */
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == CDModal)
+            {
+                cdm = new CuadroModal();
+                cdm.Owner = this; // Propietario ventana principal.
+                cdm.ShowDialog();
+
+                if (cdm.DialogResult == true)
+                { 
+                 // Cuando aceptas
+                }
+            }
+        }
+        /* FIN METODOS CUADRO MODAL */
     }
 }
